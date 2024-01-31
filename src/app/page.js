29d4aa1d7a7375d9f27bfd577c90944774a2dfb4
Ubuntu <ubuntu@ip-172-29-177-32.us-east-2.compute.internal>
2024-01-31 21:58:35 +0000

    update homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 export default function HomePage() {
   // throw new Error('Error on purpose')
   return (
-<>
-<h1> Home Page</h1>
-
-</>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.description}>
+          Esse consequat enim Lorem occaecat id minim enim. Sint sit ad ad
+          laborum officia ipsum Lorem do enim. Eu voluptate reprehenderit ut
+          laborum nostrud irure irure incididunt ullamco duis et amet laboris.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+            <Image src='/brands.png' alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
   );
 }
