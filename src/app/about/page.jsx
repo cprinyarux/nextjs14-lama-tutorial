08bf27467096ee16c800375cbbba2bc3f6a6ca8b
Chanawee <chanawee.prinyarux@gmail.com>
2024-01-31 06:21:50 -0600

    update mobile menu

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Image from 'next/image'
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <Image src="/about.png" alt="about" width={500} height={500}/>
+    </div>
   )
 }
 
